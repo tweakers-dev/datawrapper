75162a88fe37f0f1ad94ef5926a467133b8abda5
Gregor Aisch <contact@vis4.net>
2013-12-27 18:13:21 +0100

    showing annotate step (formerly known as "tell the story") in main editor nav

--- lib/utils/add_editor_nav.php
+++ lib/utils/add_editor_nav.php
@@ -5,8 +5,9 @@ function add_editor_nav(&$page, $step) {
     $steps = array();
     $steps[] = array('index'=>1, 'id'=>'upload', 'title'=>__('Upload Data'));
     $steps[] = array('index'=>2, 'id'=>'describe', 'title'=>__('Check & Describe'));
-    $steps[] = array('index'=>3, 'id'=>'visualize', 'title'=>__('Visualize'));
-    $steps[] = array('index'=>4, 'id'=>'publish', 'title'=>__('Publish & Embed'));
+    $steps[] = array('index'=>3, 'id'=>'visualize#refine', 'title'=>__('Visualize'));
+    $steps[] = array('index'=>4, 'id'=>'visualize#annotate', 'title'=>__('Annotate'));
+    $steps[] = array('index'=>5, 'id'=>'publish', 'title'=>__('Publish & Embed'));
     $page['steps'] = $steps;
     $page['chartLocale'] = $page['locale'];
     $page['metricPrefix'] = get_metric_prefix($page['chartLocale']);
