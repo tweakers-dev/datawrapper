6ddb593a3b31e92cb6e4d55abb728252641d3b8f
Gregor Aisch <contact@vis4.net>
2014-01-03 10:50:20 +0100

    refined editor steps wording

--- lib/utils/add_editor_nav.php
+++ lib/utils/add_editor_nav.php
@@ -3,11 +3,11 @@
 function add_editor_nav(&$page, $step, $chart) {
     // define 4 step navigation
     $steps = array();
-    $steps[] = array('index'=>1, 'id'=>'upload', 'title'=>__('Upload Data'));
-    $steps[] = array('index'=>2, 'id'=>'describe', 'title'=>__('Check & Describe'));
+    $steps[] = array('index'=>1, 'id'=>'upload', 'title'=>__('Upload'));
+    $steps[] = array('index'=>2, 'id'=>'describe', 'title'=>__('Check'));
     $steps[] = array('index'=>3, 'id'=>'visualize#refine', 'title'=>__('Visualize'));
     $steps[] = array('index'=>4, 'id'=>'visualize#annotate', 'title'=>__('Annotate'));
-    $steps[] = array('index'=>5, 'id'=>'publish', 'title'=>__('Publish & Embed'));
+    $steps[] = array('index'=>5, 'id'=>'publish', 'title'=>__('Embed'));
     $page['steps'] = $steps;
     $page['chartLocale'] = $page['locale'];
     $page['metricPrefix'] = get_metric_prefix($page['chartLocale']);
