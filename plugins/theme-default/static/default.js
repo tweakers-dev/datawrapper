115dc43c046ed5be2dcacf21f4c1a9acdf1f1ab0
Arjen <acm@tweakers.net>
2014-09-30 08:43:14 +0200

    Change default colors to represent both a larger set of colors and be better suited to vision impaired visitors

--- plugins/theme-default/static/default.js
+++ plugins/theme-default/static/default.js
@@ -7,7 +7,29 @@
 
         colors: {
             // primary colors
-            palette: ["#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd"],
+			// Original palette:
+			/*
+            palette: [
+            	"#1f77b4", // Blue
+            	"#ff7f0e", // Orange
+            	"#2ca02c", // Green
+            	"#d62728", // Red
+            	"#9467bd"  // Purple
+            	],
+			*/
+            palette: [
+				// Colors decided by Femme, but based upon research of SRON's technical note (SRON/EPS/TN/09-002)
+				"#98bc1c", // Groen
+				"#0cab92", // Turqoise
+				"#8be0eb", // Lichtblauw
+				"#1f66bb", // Donkerblauw
+				"#ede56a", // Geel
+				"#bd733b", // Bruin
+				"#b576a1", // Paars
+				"#b81b3b", // Rood
+				"#f9aa68", // Zalm
+				"#c5c8c8", // Grijs
+			],
             // secondary colors, used in custom color dialog
             // this should contain colors that might be useful
             secondary: ["#000000", '#777777', '#cccccc', '#ffd500', '#6FAA12'],
